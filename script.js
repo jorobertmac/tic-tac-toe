f4ab53ab9fe14116446a7bb08b21df122983fbe5
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-08-16 12:46:27 -0700

    Initialize main game play logic

--- script.js
+++ script.js
@@ -51,10 +51,89 @@ const Player = (function () {
   }
 })()
 
-// const Game = (function () {
-//   return {
-//     playerX = Player.create(nameX, "X")
-//     playerO = Player.create(nameO, "O")
+const Game = (function () {
+  let playerX
+  let playerO
+  let playersTurn
+  let gameOver
 
-//   }
-// })()
+  const boardLocation = (location) => {
+    switch (location) {
+      case "1":
+        return {row: 0, column: 0};
+      case "2":
+        return {row: 0, column: 1};
+      case "3":
+        return {row: 0, column: 2};
+      case "4":
+        return {row: 1, column: 0};
+      case "5":
+        return {row: 1, column: 1};
+      case "6":
+        return {row: 1, column: 2};
+      case "7":
+        return {row: 2, column: 0};
+      case "8":
+        return {row: 2, column: 1};
+      case "9":
+        return {row: 2, column: 2};
+      default:
+        break;
+    }
+  }
+
+  const initialize = (function () {
+    playerX = Player.create(prompt("Name of Player X"), "X")
+    playerO = Player.create(prompt("Name of Player O"), "O")
+    playersTurn = playerX
+    gameOver = false
+    Board.reset()
+    Board.displayBoard()
+  })
+
+  const turn = function () {
+    const mark = prompt(`${playersTurn.playerName}'s turn!\nWhat space do you want to mark with ${playersTurn.playerSign}`)
+    const location = boardLocation(mark)
+    if (Board.isEmpty(location.row, location.column)) {
+      Board.updateCell(location.row, location.column, playersTurn.playerSign)
+    }
+  }
+
+  const checkGameOver = () => {
+    const b = Board.getCurrentState()
+    if (
+      (b[0][0] !== null && b[0][0] === b[0][1] && b[0][0] === b[0][2]) || // Row 1
+      (b[1][0] !== null && b[1][0] === b[1][1] && b[1][0] === b[1][2]) || // Row 2
+      (b[2][0] !== null && b[2][0] === b[2][1] && b[2][0] === b[2][2]) || // Row 3
+
+      (b[0][0] !== null && b[0][0] === b[1][0] && b[0][0] === b[2][0]) || // Column 1
+      (b[0][1] !== null && b[0][1] === b[1][1] && b[0][1] === b[2][1]) || // Column 2
+      (b[0][2] !== null && b[0][2] === b[1][2] && b[0][2] === b[2][2]) || // Column 3
+
+      (b[0][0] !== null && b[0][0] === b[1][1] && b[0][0] === b[2][2]) || // Diag. 159
+      (b[0][2] !== null && b[0][2] === b[1][1] && b[0][2] === b[2][0])    // Diag. 357
+    ) {
+      gameOver = true
+    }
+  }
+
+  const changePlayersTurn = function () {
+    playersTurn === playerX ? playersTurn = playerO : playersTurn = playerX
+  }
+
+  const run = function () {
+    initialize()
+    while (!gameOver) {
+      turn()
+      Board.displayBoard()
+      checkGameOver()
+      changePlayersTurn()
+    }
+    alert(`Game Over!`)
+  }
+  
+  
+  return {
+    run
+  }
+})()
